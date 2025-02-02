8b8fc411133dbeb5bfbc96c0bc050e1059d073cf
francisco <franciscodagamac@gmail.com>
2025-02-02 12:29:33 +0800

    added header and logo image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 50) {
+            $('header').addClass('shrink');
+        } else {
+            $('header').removeClass('shrink');
+        }
+
+        $('.section').each(function() {
+            var sectionTop = $(this).offset().top - $('header').outerHeight();
+            var sectionBottom = sectionTop + $(this).outerHeight();
+            var scrollPosition = $(window).scrollTop();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                var currentId = $(this).attr('id');
+                $('nav a.header-a').removeClass('active');
+                $('nav a.header-a[href="#' + currentId + '"]').addClass('active');
+            }
+        });
+    });
+
+    $('header nav a.header-a').on('click', function(e) {
+        e.preventDefault();
+        $('header nav a.header-a').removeClass('active');
+        $(this).addClass('active');
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 600);
+    });
+
+    $('.menu-toggle').on('click', function() {
+        $('nav').toggleClass('active');
+        $(this).html($(this).text() === '☰' ? '✖' : '☰');
+    });
 
     function resizeWindow() {
         if ($(window).width() <= 1024) {
