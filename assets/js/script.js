383d655fb3b522b790d9baaaa377d1315870c3d2
francisco <franciscodagamac@gmail.com>
2025-03-11 15:11:39 +0800

    updated the owl carousel nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,11 +39,16 @@ $(document).ready(function() {
         if ($(window).width() <= 1024) {
             $('.panel.my-services .columns').addClass('owl-carousel owl-theme').owlCarousel({
                 margin: 60,
-                nav: false,
+                nav: true,
+                navText: [
+                    '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_62_1011)"><path d="M9.28894 14.3291L19.0889 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.28894 14.3291L19.0889 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_62_1011"><rect width="28" height="28" fill="white" transform="translate(0.888916 0.329102)"/></clipPath></defs></svg>',
+                    '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.7111 14.3291L9.91106 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M19.7111 14.3291L9.91106 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+                ],
                 stagePadding: 80,
                 responsive: {
                     0: {
                         items: 1,
+                        stagePadding: 0,
                     },
                     600: {
                         items: 2,
@@ -53,7 +58,11 @@ $(document).ready(function() {
 
             $('.panel.skills .container').addClass('owl-carousel owl-theme').owlCarousel({
                 margin: 60,
-                nav: false,
+                nav: true,
+                navText: [
+                    '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_62_1011)"><path d="M9.28894 14.3291L19.0889 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.28894 14.3291L19.0889 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_62_1011"><rect width="28" height="28" fill="white" transform="translate(0.888916 0.329102)"/></clipPath></defs></svg>',
+                    '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.7111 14.3291L9.91106 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M19.7111 14.3291L9.91106 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+                ],
                 stagePadding: 80,
                 responsive: {
                     0: {
@@ -78,9 +87,13 @@ $(document).ready(function() {
     });
 
     $('.worksectioncarousel').owlCarousel({
-        loop: true,
+        // loop: true,
         margin: 60,
-        nav: false,
+        nav: true,
+        navText: [
+            '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_62_1011)"><path d="M9.28894 14.3291L19.0889 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.28894 14.3291L19.0889 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_62_1011"><rect width="28" height="28" fill="white" transform="translate(0.888916 0.329102)"/></clipPath></defs></svg>',
+            '<svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.7111 14.3291L9.91106 24.1291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/><path d="M19.7111 14.3291L9.91106 4.5291" stroke="#292929" stroke-width="1.82" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        ],
         stagePadding: 80,
         responsive: {
             0: {
